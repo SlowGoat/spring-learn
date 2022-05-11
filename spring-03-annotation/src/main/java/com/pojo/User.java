a7ffb5b7dccf5e16417ece5c4060e6ba02651561
SlowGoat <2371574887@qq.com>
2022-05-11 22:53:37 +0800

    '2022-05-11'

---
+++ spring-03-annotation/src/main/java/com/pojo/User.java
@@ -0,0 +1,16 @@
+package com.pojo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+// 等价于在xml里配置 <bean id="user" class="com.pojo.User">
+@Component
+// 设置单例或原型
+//@Scope("singleton") @Scope("prototype")
+public class User {
+
+    // 相当于bean标签里的属性注入  <property name="name" value="SlowGoat" />
+    @Value("SlowGoat")
+    public String name;
+}
